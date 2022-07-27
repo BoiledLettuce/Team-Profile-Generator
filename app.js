7ee14e3441f3d9d0b73814da7f136e25f27fec30
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-07-26 19:21:24 -0700

    added code to app.js

--- app.js
+++ app.js
@@ -12,3 +12,17 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
+
+let team = [];
+let canAddManager = true;
+
+const questions = {
+    Manager: [
+        {
+
+
+
+        },
+    ]
+}

68ee39cd582ffaa96a48d9abd040d6a56f2428cf
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-07-29 10:57:05 -0700

    starting test files

---
+++ test/Manager.test.js
@@ -0,0 +1,6 @@
+//tesing tut : https://www.youtube.com/watch?v=FgnxcUQ5vho&list=PLWu_sGNNZvopEy6di4CJCcTqqCODAgQ6B&index=1&t=635s
+
+const Manager = require("./lib/Manager");
+const Employee = require("./lib/Employee");
+
+test("Can i execute a test lol", () => {});

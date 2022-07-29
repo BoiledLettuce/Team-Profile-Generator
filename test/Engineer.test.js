f2623994c11c6bf0beab66715f78553fc4522e43
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-07-29 13:49:22 -0700

    testing engineer

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -5,3 +5,15 @@ test("github through constructor", () => {
     const e = new Engineer("Ben", 1, "ben@ben.com", testBen)
     expect(e.github).toBe(testBen);
 });
+
+test(" getGithub() should return gitBro ", () => {
+    const testBen = "gitBro"
+    const e = new Engineer("Ben", 1, "ben@ben.com", testBen);
+    expect(e.getGithub()).toBe(testBen);
+});
+
+test(" getrole() should return engineer ", () => {
+    const testBen = "Engineer";
+    const e = new Engineer("Ben", 1, "ben@ben.com", testBen);
+    expect(e.getRole()).toBe(testBen);
+});
